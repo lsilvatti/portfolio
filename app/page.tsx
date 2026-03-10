1e5334d89ccd8510c7cd65b4bf7bd7d279bb9d75
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-10 11:30:12 -0300

    feat(under-construction): Add UnderConstructionBanner component and update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@ import { Typography, Link } from "@/components/atoms";
 export default function Home() {
   return (
     <>
-      <Typography variant="h1" className="text-center">
-        🚧Under Construction
-      </Typography>
-      <Typography variant="h2" className="text-center">
-        This page is currently under construction. You can still check my connections <Link href="/connect">here</Link>.
-      </Typography>
     </>
   );
 }
---
+++ components/organisms/UnderConstructionBanner/index.tsx
@@ -0,0 +1,9 @@
+export function UnderConstructionBanner() {
+    return (
+        <div className="bg-yellow-100 text-yellow-800 px-4 py-2 rounded-md">
+            <p className="text-sm sm:text-base md:text-lg">
+                This page is currently under construction. Please check back later!
+            </p>
+        </div>
+    );
+}
