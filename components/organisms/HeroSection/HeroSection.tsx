4c8037e24ed0e049ec7d2f287ec0fce257984538
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-11 14:50:47 -0300

    feat: adjust animation delays and add TypewriterText component in HeroSection

--- components/organisms/HeroSection/HeroSection.tsx
+++ components/organisms/HeroSection/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { Typography, NameLogo, Divider } from "@/components/atoms"
+import { TypewriterText } from "@/components/molecules";
 import { useTranslations } from "next-intl";
 
 export const HeroSection = () => {
@@ -6,22 +7,30 @@ export const HeroSection = () => {
 
     return (
         <>
-            <NameLogo className={"h-6 md:h-14 opacity-0 animate-fade-down"} style={{ animationDelay: '0.2s' }} />
+            <NameLogo className={"h-6 md:h-14 opacity-0 animate-fade-down"} style={{ animationDelay: '0.1s' }} />
 
             <Typography
                 variant="h4"
-                className="text-muted-foreground opacity-0 text-center animate-fade-down mb-2"
-                style={{ animationDelay: '0.4s' }}
+                className="text-muted-foreground opacity-0 text-center animate-fade-down mb-4"
+                style={{ animationDelay: '0.2s' }}
             >
                 {t("description")}
             </Typography>
 
+            <TypewriterText 
+                prefix={t("typewriter.prefix")}
+                words={t.raw("typewriter.words") as string[]}
+                className="font-mono text-center"
+                variant="h4"
+                style={{ animationDelay: '0.3s' }}
+            />
+ 
             <Divider 
                 animated
                 color="primary"
                 size="thin"
                 className="w-[80%] md:w-full md:max-w-xl mb-4"
-                style={{ animationDelay: '1.0s' }} 
+                style={{ animationDelay: '0.4s' }} 
             />
         </>
     )
