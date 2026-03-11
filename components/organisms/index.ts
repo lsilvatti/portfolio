396192465b598901686a19f9fde7f658f9eb9a7d
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-11 14:35:26 -0300

    feat: add export for HeroSection component in index.ts

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,3 +1,4 @@
 export { UnderConstructionBanner } from "./UnderConstructionBanner";
+export { HeroSection } from "./HeroSection";
 export { Header } from "./Header";
 export { Footer } from './Footer';
