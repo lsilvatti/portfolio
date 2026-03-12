ff7cc1cdc7ae02147dfb486224caf131d0c46651
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-12 17:10:42 -0300

    feat: add Popover component with content variants and trigger support

---
+++ components/atoms/Popover/index.ts
@@ -0,0 +1,10 @@
+export {
+  Popover,
+  PopoverRoot,
+  PopoverTrigger,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverClose,
+  popoverContentVariants,
+  type PopoverContentProps,
+} from "./Popover";
