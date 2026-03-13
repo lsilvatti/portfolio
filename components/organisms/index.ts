02b87e7e9dbc38bf47b9686335c9885fdee224a5
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-13 14:20:02 -0300

    feat: add ShareButton component for sharing functionality and update exports in index files

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -2,3 +2,6 @@ export { UnderConstructionBanner } from "./UnderConstructionBanner";
 export { HeroSection } from "./HeroSection";
 export { Header } from "./Header";
 export { Footer } from './Footer';
+export { ConnectCard } from './ConnectCard';
+export { ContactForm, type ContactFormData } from './ContactForm';
+export { ConnectView } from './ConnectView';
