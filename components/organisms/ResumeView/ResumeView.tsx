f5ea10322a234871560249d84366b96f6ec63199
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-15 23:43:29 -0300

    fix: update resume file names and URLs for consistency across languages

--- components/organisms/ResumeView/ResumeView.tsx
+++ components/organisms/ResumeView/ResumeView.tsx
@@ -5,7 +5,7 @@ const mdxComponents = {
     h1: (props: any) => <Typography variant="h1" className='mb-4' {...props} />,
     h2: (props: any) => <><Typography variant="h2" className='mt-8 mb-2' {...props} /><Divider className='mb-5' /></>,
     h3: (props: any) => <Typography variant="h3" className='mb-2 font-bold' {...props} />,
-    h4: (props: any) => <Typography variant="h4" className='mb-1' {...props} />,
+    h4: (props: any) => <Typography variant="h4" className='mb-1 text-secondary-hover' {...props} />,
     p:  (props: any) => <Typography variant="body" className='mb-2' {...props} />,
     a:  (props: any) => <Link variant="primary" target='_blank' rel='noopener noreferrer' {...props} />,
     ul: (props: any) => <Typography variant="ul" className='mb-2' {...props} />,
