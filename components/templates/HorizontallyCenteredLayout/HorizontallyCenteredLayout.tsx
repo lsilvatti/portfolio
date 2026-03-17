c0b1263d1a4f0c30608c5cf580b74468351fdade
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-16 23:38:06 -0300

    feat: add HorizontallyCenteredLayout component for consistent layout structure

---
+++ components/templates/HorizontallyCenteredLayout/HorizontallyCenteredLayout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+interface HorizontallyCenteredLayoutProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export function HorizontallyCenteredLayout({ children, className }: HorizontallyCenteredLayoutProps) {
+  return (
+    <div className={`flex my-8 flex-col items-center gap-6 ${className}`}>
+      {children}
+    </div>
+  );
+}
