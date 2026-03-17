c6c7dc432c675ebafb23350e55f765bb225f2102
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-17 12:30:24 -0300

    feat: refactor NotFound component to use CenteredLayout and improve structure

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,18 +1,23 @@
 import { getTranslations } from "next-intl/server";
-import { Divider } from "@/components/atoms";
+import { Avatar, Divider, Typography } from "@/components/atoms";
 import { GoBackButton } from "@/components/molecules";
+import { CenteredLayout } from "@/components/templates/CenteredLayout/CenteredLayout";
 
 export default async function NotFound() {
   const t = await getTranslations("pages.notFound");
 
   return (
-    <div className="flex flex-1 w-full flex-col items-center justify-center gap-6">
-      <div className="flex items-center gap-6">
-        <h1 className="text-6xl font-bold">{t("title")}</h1>
-        <Divider orientation="vertical" color="border" className="h-16" />
-        <p className="text-lg text-muted-foreground">{t("description")}</p>
-      </div>
+    <CenteredLayout>
+      <Avatar  loading="eager" alt="cat photo" size="lg" src="/zelda.png" />
+
+      <Typography className="text-6xl font-bold">{t("title")}</Typography>
+
+      <Typography className="text-lg text-muted-foreground">{t("description")}</Typography>
+
+      <Divider color="border" className="max-w-70" />
+
+      <Typography variant="body" className="text-muted-foreground text-center max-w-70  sm:max-w-xl" >{t("catchphrase")}</Typography>
       <GoBackButton />
-    </div>
+    </CenteredLayout>
   );
 }
