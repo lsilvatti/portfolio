8d4ef56587ebe2a0b4a0c718c1f08cb539c8ffde
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-17 01:03:16 -0300

    feat: enhance ProjectCard component with selectable chips and additional props for filtering

--- components/molecules/ProjectCard/ProjectCard.tsx
+++ components/molecules/ProjectCard/ProjectCard.tsx
@@ -5,19 +5,51 @@ export interface ProjectCardProps {
     description: string; 
     languages: string[];
     tags: string[] | null;
+    style?: React.CSSProperties;
+    filteredLanguages?: string[];
+    filteredTags?: string[];
+    onClickLanguageChip?: (language: string) => void;
+    onClickTagChip?: (tag: string) => void;
 }
 
-export function ProjectCard({ title, description, languages, tags }: ProjectCardProps) {
+export function ProjectCard({ 
+  title, 
+  description, 
+  languages, 
+  tags, 
+  style, 
+  filteredLanguages, 
+  filteredTags, 
+  onClickLanguageChip, 
+  onClickTagChip 
+}: ProjectCardProps) {
     return (
-        <Card key={title} className="flex flex-col border animate-fade-pop-in gap-3 p-4 hover:border-primary hover:cursor-pointer hover:bg-surface-hover" style={{ animationDelay: '0.1s' }}>
-            <Typography variant="h3" >{title.replace(/-/g, ' ')}</Typography>
+        <Card 
+          className="flex flex-col border animate-fade-pop-in gap-3 p-4 hover:border-primary hover:cursor-pointer hover:bg-surface-hover w-full" 
+          style={{ animationDelay: '0.1s', ...style }}
+        >
+            <Typography variant="h3">{title.replace(/-/g, ' ')}</Typography>
             <Typography variant="body">{description}</Typography>
             <div className="flex gap-2 flex-wrap">
             {languages.map((lang) => (
-                <Chip key={lang} label={lang} variant="primary" className="mb-2" />
+                <Chip 
+                  key={lang} 
+                  selectable 
+                  label={lang} 
+                  variant={filteredLanguages?.includes(lang) ? 'primary' : 'outline-primary'} 
+                  className="mb-2" 
+                  onClick={() => onClickLanguageChip?.(lang)} 
+                />
             ))}
             {tags && tags.map((tag) => (
-                <Chip key={tag} label={tag} variant="secondary" className="mb-2" />
+                <Chip 
+                  key={tag} 
+                  selectable 
+                  label={tag} 
+                  variant={filteredTags?.includes(tag) ? 'secondary' : 'outline-secondary'} 
+                  className="mb-2" 
+                  onClick={() => onClickTagChip?.(tag)} 
+                />
             ))}
             </div>
         </Card>
