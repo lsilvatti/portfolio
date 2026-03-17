c036e60a6ded55a7b9b695259b438177d6f06eca
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-17 01:03:24 -0300

    fix: ensure className is conditionally applied in HorizontallyCenteredLayout component

--- components/templates/HorizontallyCenteredLayout/HorizontallyCenteredLayout.tsx
+++ components/templates/HorizontallyCenteredLayout/HorizontallyCenteredLayout.tsx
@@ -7,7 +7,7 @@ interface HorizontallyCenteredLayoutProps {
 
 export function HorizontallyCenteredLayout({ children, className }: HorizontallyCenteredLayoutProps) {
   return (
-    <div className={`flex my-8 flex-col items-center gap-6 ${className}`}>
+    <div className={`flex my-8 flex-col items-center gap-6 ${className ? className : ''}`}>
       {children}
     </div>
   );
